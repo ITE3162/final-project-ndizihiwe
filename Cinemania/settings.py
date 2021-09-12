7f6b59d24c523a189ef8c372981920debd5a3b89
Simon Pierre NDIZIHIWE <ndizihiwesimon@gmail.com>
2021-09-12 22:58:50 +0200

    Update settings.py

--- Cinemania/settings.py
+++ Cinemania/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Location for static files root folder
 STATICFILES_DIRS = [
