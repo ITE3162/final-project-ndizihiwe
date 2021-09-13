8cc73d064a583f7e1748cdf0e944841e2f119c90
Simon Pierre NDIZIHIWE <ndizihiwesimon@gmail.com>
2021-09-14 01:48:25 +0200

    Reregistered blog

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from Dashboard.models import User
+from Dashboard.models import User, Blog
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Blog)
