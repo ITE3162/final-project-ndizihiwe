b12010b90c7a91e18f559644ed16c40928595e65
Simon Pierre NDIZIHIWE <ndizihiwesimon@gmail.com>
2021-09-13 03:07:47 +0200

    added staticfiles

--- Cinemania/settings.py
+++ Cinemania/settings.py
@@ -124,14 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
 
-# Location for static files root folder
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 
